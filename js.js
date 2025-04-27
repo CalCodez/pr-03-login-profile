d1e663f56c87da3e968fbe81cb8b899f4b905e98
Calvin <calcodez@outlook.com>
2025-04-27 17:53:01 -0500

    fly complete

--- js.js
+++ js.js
@@ -13,3 +13,39 @@ const click = 'click';
 const keyup = 'keyup';
 const flexActive = 'flex-active';
 const flexInactive = 'flex-inactive';
+
+const userName = getById('user-name');
+const password = getById('password');
+const logInBtn = getById('login-btn');
+const toastContainer = select('.toast-container');
+const closeToastBtn = select('#close-toast-btn');
+const errorContainer = select('.error-container');
+const errorExit = getById('error-exit');
+const errorMsgContainer = select('.error-msg-container');
+
+logInBtn.addEventListener(click, () => {
+	if (!userName.value == '' && !password.value == '') {
+		toggleClass(toastContainer, flexActive);
+	} else if (!userName.value == '' && password.value == '') {
+		toggleClass(errorContainer, flexActive);
+		textContent(errorMsgContainer, 'PLEASE ENTER YOUR PASSWORD!!!!');
+	} else if (userName.value == '' && !password.value == '') {
+		toggleClass(errorContainer, flexActive);
+		textContent(errorMsgContainer, 'ENTER YOUR USERNAME!!!!');
+	} else if (userName.value == '' && password.value == '') {
+		toggleClass(errorContainer, flexActive);
+		textContent(errorMsgContainer, 'ENTER USERNAME AND PASSWORD.. DUH!!!');
+	}
+
+	closeToastBtn.addEventListener(click, () => {
+		if (toastContainer.classList.contains(flexActive)) {
+			toggleClass(toastContainer, flexActive);
+		}
+	});
+
+	errorExit.addEventListener(click, () => {
+		if (errorContainer.classList.contains(flexActive)) {
+			toggleClass(errorContainer, flexActive);
+		}
+	});
+});
